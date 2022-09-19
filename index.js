ac553cd5f72a69e94182a2ec68af6ea172eb424b
akshay643 <73953645+akshay643@users.noreply.github.com>
2022-09-19 14:25:07 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -9,3 +9,22 @@ function menuToggle() {
 function scollToTop() {
   document.body.scrollTop = document.documentElement.scrollTop = 0
 }
+
+//
+
+let slideIndex = 0
+showSlides()
+
+function showSlides() {
+  let i
+  let slides = document.getElementsByClassName('s2-slides')
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none'
+  }
+  slideIndex++
+  if (slideIndex > slides.length) {
+    slideIndex = 1
+  }
+  slides[slideIndex - 1].style.display = 'block'
+  setTimeout(showSlides, 3000) // Change image every 2 seconds
+}
